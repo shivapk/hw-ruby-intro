79b34bd1fbc54667eb77287ad3f785955deae932
Shiva Kumar Pentyala <shivakumar.pentyala@gmail.com>
2019-09-19 11:33:03 -0500

    update class methpd

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,95 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |e|
+    sum += e
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0 then
+    return 0
+  elsif arr.length == 1 then
+    return arr[0]
+  end
+
+  arr.sort!
+  arr.reverse!
+  return arr[0] + arr[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.sort!
+  i = 0
+  j = arr.length-1
+  while i < j
+    sum = arr[i] + arr[j]
+    if sum == n then
+      return true
+    elsif sum < n then
+      i += 1
+    else
+      j -= 1
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /^[b-df-hj-np-tv-z]/i then true else false end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0 then return false end
+  s = "00" + s
+  # for any binary number shifting by right is dividing by 2, double shifting twice is dividing by 4
+  return s =~ /^[01]*00$/
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.length == 0 || price <= 0 then
+      raise ArgumentError.new("isbn shouldn't be empty nor price should be less than zero")
+    end
+    @isbn, @price = isbn, price
+  end
+
+  # getters
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
+  # setters
+  def isbn=(isbn)
+    if isbn.length == 0 then
+      raise ArgumentError.new("isbn shouldn't be empty")
+    end
+    @isbn = isbn
+  end
+
+  def price=(price)
+    if price <= 0 then
+      raise ArgumentError.new("price shouldn't be negative nor zero number")
+    end
+    @price = price
+  end
+
+  # format price as two decimal point string
+  def price_as_string
+    return "$%0.2f" % @price
+  end
 end
